cc277298c73c168e76d09f97345e2317f92793b7
NamingIsDifficult <kelsuzad@126.com>
2014-07-29 17:46:21 +0800

    Update jquery-draggable.js
    1,fix bug in ie7/ie6
    2,add new config to enable moving specified dom elememt while you drag the current dom element

--- jquery-draggable.js
+++ jquery-draggable.js
@@ -1,6 +1,7 @@
 /*Settings:
         DragContainer:仅在该Dom区域内允许拖拽   
         DragDirection{"Both":允许x/y轴方向拖拽,"Vertical":允许y轴方向拖拽,"Horizontal":允许x轴方向拖拽}  
+        ItemMovesWhenDragging:mousedown激活拖动后，mousemove时实际在页面上移动的dom
 */
 
 jQuery.draggable = {
@@ -9,12 +10,16 @@ jQuery.draggable = {
         this.each(function () {
             this.dragSettings = jQuery.extend({
                 DragContainer: $("body"),
-                DragDirection: "Both"
+                DragDirection: "Both",
+                ItemMovesWhenDragging: "self"
             }, settings || {});
             jQuery.draggable.MakeDraggable(this);
             jQuery.draggable.Register.push(this);
         });
-        document.onmouseup = function () { jQuery.draggable.DragDisabled(); };
+        document.onmouseup = function () { 
+            window.getSelection();
+            jQuery.draggable.DragDisabled(); 
+        };
         return this;
     },
     MakeDraggable: function (draggableObj) {
@@ -22,31 +27,36 @@ jQuery.draggable = {
             jQuery.draggable.DragEnabled(draggableObj, e);
             jQuery.draggable.StopBubble(e);
         };
-        draggableObj.style.position = "absolute";
+        var movingItem = draggableObj.dragSettings.ItemMovesWhenDragging === "self" ? draggableObj : draggableObj.dragSettings.ItemMovesWhenDragging;
+        $(movingItem).css("position","absolute");
     },
-    DragEnabled: function (draggableObj, e) {
-        draggableObj.canMove = true;
+    DragEnabled: function(draggableObj, e) {
+        draggableObj.canMove = true; 
+        var movingItem = draggableObj.dragSettings.ItemMovesWhenDragging === "self" ? draggableObj : draggableObj.dragSettings.ItemMovesWhenDragging;
         var event = e || window.event;
         var xcoordinates = event.clientX > 0 ? event.clientX : 0;
         var ycoordinates = event.clientY > 0 ? event.clientY : 0;
-        var xcurrentPosition = $(draggableObj).css("left");
-        var ycurrentPosition = $(draggableObj).css("top");
+        var xcurrentPosition = $(movingItem).css("left");
+        var ycurrentPosition = $(movingItem).css("top");
         xcurrentPosition = xcurrentPosition == "auto" ? "0" : xcurrentPosition.replace("px", "");
         ycurrentPosition = ycurrentPosition == "auto" ? "0" : ycurrentPosition.replace("px", "");
         draggableObj.xcompansation = xcoordinates - xcurrentPosition;
         draggableObj.ycompansation = ycoordinates - ycurrentPosition;
 
         var topLimitationMin = 0;
-        var topLimitationMax = draggableObj.dragSettings.DragContainer.height()-$(draggableObj).height();
+        var topLimitationMax = draggableObj.dragSettings.DragContainer.height() - $(movingItem).height();
         var leftLimitationMin = 0;
-        var leftLimitationMax = draggableObj.dragSettings.DragContainer.width()-$(draggableObj).width();
-        var limitationConfig= {
+        var leftLimitationMax = draggableObj.dragSettings.DragContainer.width() - $(movingItem).width();
+        var limitationConfig = {
             TopLimitationMin: topLimitationMin,
             TopLimitationMax: topLimitationMax,
             LeftLimitationMin: leftLimitationMin,
-            LeftLimitationMax: leftLimitationMax,
-        }
-        document.onmousemove = function (e) { jQuery.draggable.DragExcuting(draggableObj,limitationConfig, e); };
+            LeftLimitationMax: leftLimitationMax
+        };
+        document.onmousemove = function(e) {
+            window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+            jQuery.draggable.DragExcuting(draggableObj, $(movingItem), limitationConfig, e);
+        };
     },
     DragDisabled: function () {
         if (jQuery.draggable.Register.length > 0) {
@@ -59,7 +69,7 @@ jQuery.draggable = {
             jQuery.draggable.StopBubble(e);
         };
     },
-    DragExcuting: function (draggableObj,limitationConfig, e) {
+    DragExcuting: function (draggableObj,movingItem, limitationConfig, e) {
         var event = e || window.event;
         var xcoordinates = event.clientX > 0 ? event.clientX : 0;
         var ycoordinates = event.clientY > 0 ? event.clientY : 0;
@@ -69,20 +79,22 @@ jQuery.draggable = {
             switch (draggableObj.dragSettings.DragDirection) {
                 case "Both":
                     if (limitationConfig.LeftLimitationMin < left && left < limitationConfig.LeftLimitationMax) {
-                        draggableObj.style.left = left + "px";
+                        movingItem.css("left", left + "px");
                     }
                     if (limitationConfig.TopLimitationMin < top && top < limitationConfig.TopLimitationMax) {
-                        draggableObj.style.top = top + "px";
+                        movingItem.css("top", top + "px");
                     }
                     break;
                 case "Vertical":
                     if (limitationConfig.TopLimitationMin < top && top < limitationConfig.TopLimitationMax) {
-                        draggableObj.style.top = ycoordinates - draggableObj.ycompansation + "px";
+                        movingItem.css("left", movingItem.css("left"));
+                        movingItem.css("top", top + "px");
                     }
                     break;
                 case "Horizontal":
                     if (limitationConfig.LeftLimitationMin < left && left < limitationConfig.LeftLimitationMax) {
-                        draggableObj.style.left = left + "px";
+                        movingItem.css("top", movingItem.css("top"));
+                        movingItem.css("left", left + "px");
                     }
                     break;
                 default:
